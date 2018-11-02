27242b5275752d42026cc301f71b7439a4703378
Ayush Kaushal <ayushk4@gmail.com>
2018-11-02 17:51:31 +0530

    Add script to get course data

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import time
+from twerp import twerpUpdate
+def getDept():
+    r = requests.get('https://hercules-10496.herokuapp.com/api/v1/department/info/all')
+    #departments = {}
+    departments = r.json()
+    return departments
+
+def getCoursesDeptWise(deptCode):
+    r = requests.get('https://hercules-10496.herokuapp.com/api/v1/course/info/department/{0}'.format(deptCode))
+    courses = {}
+    courses = r.json()
+    return courses
+
+def getCourseData(course):
+    r = requests.get('https://hercules-10496.herokuapp.com/api/v1/course/timetable/{0}'.format(course['code']))
+    courseData = r.json()
+    return courseData
+
+#def twerp():
+
+def main():
+    departments = getDept()
+    for department in departments:
+        courses = getCoursesDeptWise(department['code'])
+        for course in courses:
+            courseData = getCourseData(course)
+            #twerp(courseData)
+            print(courseData)
+            time.sleep(0.5)            
+
+if __name__ == '__main__':
+    main()
---
+++ twerp.py
@@ -0,0 +1,6 @@
+#import pywikibot
+
+data ={'Monday': [{'course': {'name': 'TRANSFER PROCESS IN FOOD ENGINEERING', 'code': 'AG60097', 'credits': 4}, 'slot': {'time': {'day': 'Monday', 'time': '8 AM'}, 'slot': 'A3'}, 'rooms': ['0']}, {'course': {'name': 'TRANSFER PROCESS IN FOOD ENGINEERING', 'code': 'AG60097', 'credits': 4}, 'slot': {'time': {'day': 'Monday', 'time': '9 AM'}, 'slot': 'A3'}, 'rooms': ['0']}, {'course': {'name': 'TRANSFER PROCESS IN FOOD ENGINEERING', 'code': 'AG60097', 'credits': 4}, 'slot': {'time': {'day': 'Monday', 'time': '5 PM'}, 'slot': 'S3'}, 'rooms': ['0']}], 'Tuesday': [{'course': {'name': 'TRANSFER PROCESS IN FOOD ENGINEERING', 'code': 'AG60097', 'credits': 4}, 'slot': {'time': {'day': 'Tuesday', 'time': '12 PM'}, 'slot': 'A3'}, 'rooms': ['0']}], 'Wednesday': None, 'Thursday': [{'course': {'name': 'TRANSFER PROCESS IN FOOD ENGINEERING', 'code': 'AG60097', 'credits': 4}, 'slot': {'time': {'day': 'Thursday', 'time': '5 PM '}, 'slot': 'S3'}, 'rooms': ['0']}], 'Friday': [{'course': {'name': 'TRANSFER PROCESS IN FOOD ENGINEERING', 'code': 'AG60097', 'credits': 4}, 'slot': {'time': {'day': 'Friday', 'time': '5 PM'}, 'slot': 'S3'}, 'rooms': ['0']}]}
+
+def twerpUpdate():
+    pass
