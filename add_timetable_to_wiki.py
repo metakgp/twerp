7b5593a6adc29490835165b76d6a66e13c6d018d
beserker1 <aaryanbhagat377@gmail.com>
2019-06-10 19:24:46 +0530

    modified twerp and add_timetable_to_wiki acc to pep8

--- add_timetable_to_wiki.py
+++ add_timetable_to_wiki.py
@@ -1,17 +1,17 @@
 import pywikibot
 
-def update_with_timetable(allcoursesonwiki,site,course,timetable):
+
+def update_with_timetable(allcoursesonwiki, site, course, timetable):
     course_page = allcoursesonwiki[course['code']]
     if '=Time Table=' in course_page.text:
         course_page.text = course_page.text.rsplit('=Time Table=', 1)[0] + "\n\n=Time Table=\n\n" + timetable + "\n"
     else:
         course_page.text = course_page.text + "\n\n=Time Table=\n\n" + timetable + "\n"
 
-    
     course_page.save(('Add Time Table'))
 
 
-def update_without_timetable(allcoursesonwiki,site,course):
+def update_without_timetable(allcoursesonwiki, site, course):
     course_page = allcoursesonwiki[course]
     if '=Time Table=' in course_page.text:
         course_page.text = course_page.text.rsplit('=Time Table=', 1)[0] + "\n\n"
@@ -20,12 +20,12 @@ def update_without_timetable(allcoursesonwiki,site,course):
     else:
         pass
 
-def create_course_page(site,course,timetable):
+
+def create_course_page(site, course, timetable):
 
     course['timetable'] = timetable
 
 # NOTE: The braces are doubled to escape it as string formatting character
-    
     course_text = r"""
 {{{{Infobox course
 | course_code = {code}
@@ -57,8 +57,8 @@ def create_course_page(site,course,timetable):
 {timetable}
     """
     course_text = course_text.format(**course)
-    course_page = pywikibot.Page(site, "{}: {}".format(course['code'], course['name']))
+    course_page = pywikibot.Page(site, "{}: {}".format(course['code'], 
+                                                       course['name']))
     course_page.text = course_text
     msg = "Create Page for {}".format(course['name'])
     course_page.save(msg)
-
--- twerp.py
+++ twerp.py
@@ -1,7 +1,7 @@
 import os
 import sys
 path = os.path.abspath(os.path.dirname(__file__))
-PATH_TO_MODULE=os.path.join(path,'wiki_files')
+PATH_TO_MODULE = os.path.join(path, 'wiki_files')
 sys.path.append(PATH_TO_MODULE)
 
 
@@ -9,21 +9,22 @@ import pywikibot
 from pywikibot import pagegenerators
 import string
 
-from course_details import getDeptList,getCourseData,getCoursesDeptWise
+from course_details import getDeptList, getCourseData, getCoursesDeptWise
 from time_table_to_wiki_table import getTimeTable
 from add_timetable_to_wiki import create_course_page, update_with_timetable, update_without_timetable
-from prof_details import get_all_info_of_faculty,get_table_of_prof
-from update_prof_table import check_profpage_existence,make_Time_Table_of_prof, update_profpage_with_timetable
+from prof_details import get_all_info_of_faculty, get_table_of_prof
+from update_prof_table import check_profpage_existence, make_Time_Table_of_prof, update_profpage_with_timetable
 
 site = pywikibot.Site()
 
-def course_department_wise(allcoursesonwiki,existing_course_pages):
+
+def course_department_wise(allcoursesonwiki, existing_course_pages):
     departments = getDeptList()
     course_list_this_sem = []
     new_pages_created = []
 
     for department in departments:
-        print('Courses from ' + department['name'] )
+        print('Courses from ' + department['name'])
         courses = getCoursesDeptWise(department['code'])
         for course in courses:
             if course['code'] == 'EA10005':
@@ -34,21 +35,20 @@ def course_department_wise(allcoursesonwiki,existing_course_pages):
                 new_pages_created.append(course['code'])
                 course['department'] = department['name']
                 timetable = getTimeTable(getCourseData(course))
-                create_course_page(site,course,timetable)
+                create_course_page(site, course, timetable)
             else:
                 timetable = getTimeTable(getCourseData(course))
-                update_with_timetable(allcoursesonwiki,site,course,timetable)
+                update_with_timetable(allcoursesonwiki, site, course,
+                                      timetable)
 
             course_list_this_sem.append(course['code'])
 
     print('\n\nFound ' + str(len(course_list_this_sem)) + ' courses being offered this semester')
-    not_this_sem_courses  = [i for i in allcoursesonwiki if i not in course_list_this_sem]
-
-
+    not_this_sem_courses = [i for i in allcoursesonwiki if i not in course_list_this_sem]
     print('Now removing previous year\'s timetable from the courses not in this semester')
     for course in not_this_sem_courses:
         print(allcoursesonwiki[course])
-        update_without_timetable(allcoursesonwiki,site,course)
+        update_without_timetable(allcoursesonwiki, site, course)
 
     print('New pages Created = ' + str(len(new_pages_created)))
     print('Courses this semester = ' + str(len(course_list_this_sem)))
@@ -57,17 +57,19 @@ def course_department_wise(allcoursesonwiki,existing_course_pages):
 
 #      TODO : WRITE THE NAME OF THE CURRENT SEM AS HEADING TO THE TIMETABLE
 def update_time_table_of_prof():
-    info_table=get_all_info_of_faculty()
+    info_table = get_all_info_of_faculty()
     for entry in info_table:
-        table_json=get_table_of_prof(entry['name'],entry['department']['code'])
-        timetable=make_Time_Table_of_prof(table_json)
-        prof_existence=check_profpage_existence(entry['name'])
-        if(prof_existence==1):#1 means not exist
-            print("Page of professor {} not found skipping".format(entry['name']))
+        table_json = get_table_of_prof(entry['name'], entry['department']['code'])
+        timetable = make_Time_Table_of_prof(table_json)
+        prof_existence = check_profpage_existence(entry['name'])
+        if(prof_existence == 1):  # 1 means not exist
+            print("Page of professor {} not found skipping".format(
+                   entry['name']))
         else:
             print('Professor {} page found'.format(entry['name']))
-            update_profpage_with_timetable(entry,site,timetable)
+            update_profpage_with_timetable(entry, site, timetable)
         print("Updated time table of {}".format(entry['name']))
+
 
 def main():
 
@@ -75,35 +77,28 @@ def main():
     print("type 1 for updating course page with timetable")
     print("type 2 for updating prof page with timetable")
     print("type 3 for doing both of the above")
-    answer=input('>')
-    if(answer=='1'):
-        cat = pywikibot.Category(site,'Category:Courses')
+    answer = input('>')
+    if(answer == '1'):
+        cat = pywikibot.Category(site, 'Category:Courses')
         gen = pagegenerators.CategorizedPageGenerator(cat)
 
-        allcoursesonwiki = {i.title()[:7]:i for i in gen}
+        allcoursesonwiki = {i.title()[:7]: i for i in gen}
 
         existing_course_pages = []
         for keys in allcoursesonwiki:
             existing_course_pages.append(keys)
-        
-        course_department_wise(allcoursesonwiki,existing_course_pages)
-    
-    if(answer=='2'):
+        course_department_wise(allcoursesonwiki, existing_course_pages)
+    if(answer == '2'):
         print('Starting to update prof pages')
         update_time_table_of_prof()
-    
-    if(answer=='3'):
-        cat = pywikibot.Category(site,'Category:Courses')
+    if(answer == '3'):
+        cat = pywikibot.Category(site, 'Category:Courses')
         gen = pagegenerators.CategorizedPageGenerator(cat)
-
-        allcoursesonwiki = {i.title()[:7]:i for i in gen}
-
+        allcoursesonwiki = {i.title()[:7]: i for i in gen}
         existing_course_pages = []
         for keys in allcoursesonwiki:
             existing_course_pages.append(keys)
-        
-        course_department_wise(allcoursesonwiki,existing_course_pages)
-
+        course_department_wise(allcoursesonwiki, existing_course_pages)
         print('Starting to update prof pages')
         update_time_table_of_prof()
 
