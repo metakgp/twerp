093e51e96fa40ad2a7ee1c95947bc2a5aa6be3fb
beserker1 <aaryanbhagat377@gmail.com>
2019-06-10 19:31:01 +0530

    modified add time_table_to_wiki_table acc to pep8

--- time_table_to_wiki_table.py
+++ time_table_to_wiki_table.py
@@ -1,10 +1,11 @@
 import os
 path = os.path.abspath(os.path.dirname(__file__))
 
-def createTimeTableFromSlots(slots,venues):
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday']
-    hours = ['8','9','10','11','12','LunchBreak','2','3','4','5']
+def createTimeTableFromSlots(slots, venues):
+
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    hours = ['8', '9', '10', '11', '12', 'LunchBreak', '2', '3', '4', '5']
 
     timetable = '{| class="wikitable \n|-\n! Day !! 8:00-8:55 am !! 9:00-9:55 am !! 10:00-10:55 am !! 11:00-11:55 am !! 12:00-12:55 pm !!  !! 2:00-2:55 pm !! 3:00-3:55 pm!! 4:00-4:55 pm!! 5:00-5:55 pm'
     currentSlotIndex = 0
@@ -21,13 +22,14 @@ def createTimeTableFromSlots(slots,venues):
     timetable += '\n|}'
     return timetable
 
+
 def getTimeTable(data):
 
     slots = []
     venues = []
 
     for day in data:
-        if(data[day] != None):
+        if data[day] is not None:
             for element in data[day]:
                 slot = {}
                 slot['time'] = element['slot']['time']['time'].split(' ', 1)[0]
@@ -35,7 +37,7 @@ def getTimeTable(data):
                 slots.append(slot)
                 modified_room = []
                 for room in element['rooms']:
-                    if room == '0': #0 mean the class is in some room "in department"
+                    if room == '0':  # 0 mean the class is in some room "in department"
                         modified_room.append('In Dept')
                     else:
                         modified_room.append(room)
@@ -45,9 +47,9 @@ def getTimeTable(data):
     if len(slots) == 0:
         return ''
 
-    timetable = createTimeTableFromSlots(slots,venues)
+    timetable = createTimeTableFromSlots(slots, venues)
 
     if len(timetable) != 0:
         return timetable
-    else :
+    else:
         return ''
